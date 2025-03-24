5561f8291cd859a0ec21c4b0f87cb88db2439f82
rahul-ghosh-2004 <iamrahulghoshinfo@gmail.com>
2025-03-24 21:52:44 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/*
+
+1. Real world example related to the BACKEND.
+2. Explain about the folders in backend including public & src.
+3. Create a bacekend folder named "tempBacekend" and create the entire file and folder structure
+   inside it.
+
+*/
